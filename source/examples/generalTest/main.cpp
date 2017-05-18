c7f104be027ca7c6eebfd0a386b3c4e650ee73fd
Abdallah <abdou9989@gmail.com>
2017-05-17 22:18:28 -0400

    Update Project structure

---
+++ source/examples/generalTest/main.cpp
@@ -0,0 +1,12 @@
+// A simple program that computes the square root of a number
+//C++
+#include <iostream>
+
+#include <SehaProgConfig.h>
+ 
+int main (int argv, char** args)
+{
+	std::cout<< "project name: " << _PROJECT_NAME <<  std::endl ;
+	
+  return 0;
+}
